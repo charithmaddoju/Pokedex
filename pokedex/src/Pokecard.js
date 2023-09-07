e878d20c7206a6fc154aa3fae85801ee407bd253
Charith <charithmaddoju27@gmail.com>
2023-09-07 22:40:43 +0530

    create pokecard

---
+++ pokedex/src/Pokecard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Pokecard.css';
+
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+class Pokecard extends Component{
+    render(){
+        let imgSrc = `${POKE_API}${this.props.id}.png`;
+        return(
+            <div className="Pokecard">
+                <h1>{this.props.name}</h1>
+                <img src={imgSrc} alt={this.props.name}/>
+                <p>Type: {this.props.type}</p>
+                <p>EXP: {this.props.exp}</p>
+            </div>
+        );
+    };
+}
+
+export default Pokecard;
